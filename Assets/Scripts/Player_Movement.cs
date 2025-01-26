195d5a553a1bc9133413f8e48d7ef35159cdd332
baonq080921 <bao992001pth@gmail.com>
2025-01-27 03:06:55 +0700

    addJumping

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,8 +7,10 @@ public class Player_Movement : MonoBehaviour
 {
 
     [SerializeField] float speed = 200f;
+    [SerializeField] float jumpSpeed = 200f;
     private bool isFacingRight = true;
     public Rigidbody2D rb;
+    public Transform tf;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,41 +20,47 @@ public class Player_Movement : MonoBehaviour
 
     void Update(){
         // Debug.Log("Time in Update:"+Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed); // Chỉ thay đổi trục Y
+            Debug.Log("Jump pressed");
+        }
+
     }
     private void FixedUpdate() {
-        PlayerMovment();
-        Debug.Log("Time in FixedUpdate:"+Time.deltaTime);
 
+        PlayerMovment();
 
     }
 
     private void PlayerMovment(){
         float horizontal = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(horizontal,rb.linearVelocity.y,0).normalized;
-        Vector2 direction_1 = new Vector3(horizontal,rb.linearVelocity.y).normalized;
+        
+        Vector3 direction = new Vector3(horizontal,0,0).normalized;
+        // Vector2 direction_1 = new Vector3(horizontal,rb.linearVelocity.y).normalized;
 
         rb.constraints = RigidbodyConstraints2D.FreezeRotation; // Freezee the Z-axis no rotation
         
         // ======Using linerVelocity for testing character Movement: Rigibody Movement
-            rb.linearVelocity = new Vector3(direction.x * speed * Time.deltaTime, rb.linearVelocity.y,0);
+        // rb.linearVelocity = new Vector3(direction.x * speed * Time.deltaTime, rb.linearVelocity.y,0);
 
         //====Addforce====
-        //rb.AddForce(direction *speed * Time.deltaTime);
+        rb.AddForce(direction *speed * Time.deltaTime);
 
         //=== Move Positon:Using Vector 2
         //rb.MovePosition(rb.position+(direction_1 * speed * Time.deltaTime));
 
-         Debug.Log("LinearVelocity Speed: " + rb.linearVelocity.magnitude); // magnitude gives the speed
-
        
-        Debug.Log(direction);
+        
+        
+        if(direction.x > 0f && !isFacingRight){
+            Flip();
+        }
+        if(direction.x < 0f && isFacingRight){
+            Flip();
+        }
+        
 
-        if(direction.x > 0 && !isFacingRight){
-            Flip();
-        }
-        if(direction.x < 0 && isFacingRight){
-            Flip();
-        }
+        
         
     }
 
