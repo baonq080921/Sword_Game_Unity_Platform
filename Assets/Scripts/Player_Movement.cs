59d1620af5c651da6a89befafdba52c01fcd0d3d
baonq080921 <bao992001pth@gmail.com>
2025-01-27 13:30:30 +0700

    update movement

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-
-    [SerializeField] float speed = 200f;
-    [SerializeField] float jumpSpeed = 200f;
+    #region Game Variables Region
+    [SerializeField] float speed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
     private bool isFacingRight = true;
     public Rigidbody2D rb;
-    public Transform tf;
+    [SerializeField] bool isGrounded;
+    private bool isDoubleJump;
+    
+    #endregion
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log("Start ting isDoubleJump: "+ isDoubleJump);
         rb = GetComponent<Rigidbody2D>();
 
     }
 
     void Update(){
         // Debug.Log("Time in Update:"+Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed); // Chỉ thay đổi trục Y
-            Debug.Log("Jump pressed");
-        }
+       PlayerMovment();
+       Jump();
 
     }
     private void FixedUpdate() {
-
-        PlayerMovment();
-
     }
 
     private void PlayerMovment(){
@@ -41,27 +41,25 @@ public class Player_Movement : MonoBehaviour
         rb.constraints = RigidbodyConstraints2D.FreezeRotation; // Freezee the Z-axis no rotation
         
         // ======Using linerVelocity for testing character Movement: Rigibody Movement
-        // rb.linearVelocity = new Vector3(direction.x * speed * Time.deltaTime, rb.linearVelocity.y,0);
+        if(isGrounded){
+            rb.linearVelocity = new Vector3(direction.x * speed , rb.linearVelocity.y,0);
+        }else{
+            rb.linearVelocity = new Vector3(direction.x * speed * .5f , rb.linearVelocity.y,0);
+
+        }
 
         //====Addforce====
-        rb.AddForce(direction *speed * Time.deltaTime);
+        // rb.AddForce(direction *speed * Time.deltaTime);
 
         //=== Move Positon:Using Vector 2
         //rb.MovePosition(rb.position+(direction_1 * speed * Time.deltaTime));
 
-       
-        
-        
         if(direction.x > 0f && !isFacingRight){
             Flip();
         }
         if(direction.x < 0f && isFacingRight){
             Flip();
-        }
-        
-
-        
-        
+        }    
     }
 
    private void Flip(){
@@ -70,7 +68,36 @@ public class Player_Movement : MonoBehaviour
     transform.localScale = currentScale;
     isFacingRight = !isFacingRight;
     // Debug.Log(transform.localScale);
-
-
    }
+   private void Jump(){
+    bool jump = Input.GetKeyDown(KeyCode.Mouse0);
+    if(isGrounded){
+        isDoubleJump = false;
+    }
+    if(jump){
+        if(isGrounded || !isDoubleJump){
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x , jumpSpeed, 0);
+            isDoubleJump = !isDoubleJump;
+        }
+    }
+   }
+
+    // =================1st way to detect collison with the ground using Tag Compare ======================= //
+    #region OnCollisionEnter2D Region
+   private void OnCollisionEnter2D(Collision2D other) {
+    
+    if(other.gameObject.CompareTag("Ground")){
+        isGrounded = true;
+    }
+   }
+
+   private void OnCollisionExit2D(Collision2D other) {
+    if(other.gameObject.CompareTag("Ground")){
+        isGrounded = false;
+    }
+   }
+    #endregion
+    
+
+
 }
