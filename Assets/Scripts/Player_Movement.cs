073379644605a9e5fff5d5c7ae2131e11491687b
baonq080921 <bao992001pth@gmail.com>
2025-01-27 15:00:10 +0700

    fix bug on jumping

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -105,7 +105,7 @@ public class Player_Movement : MonoBehaviour
 
     private bool isGround(){
         Vector3 positiveBoxSize = new Vector3(MathF.Abs(boxSize.x),MathF.Abs(boxSize.y),0);
-        if(Physics2D.BoxCast(gameObject.transform.position,positiveBoxSize,0f,gameObject.-transform.up,castDistance,groundLayer)){
+        if(Physics2D.BoxCast(gameObject.transform.position,positiveBoxSize,0f,-gameObject.transform.up,castDistance,groundLayer)){
             return true;
         }else{
             return false;
